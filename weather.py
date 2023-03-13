c897e2c1b6a1b8c7cf7736c362b3f57037592029
vadbash <vadusikvadim@gmail.com>
2023-03-13 16:07:35 +0200

    fix pandas

--- weather.py
+++ weather.py
@@ -1,15 +1,25 @@
 import requests
 import json
 from pprint import pprint
+import pandas as pd
 
-city = input("Input a city to see the weather: ")
-
-if city == "":
-    print("please enter a city")
-    exit()
-else:
+def get_weather():
+    city = input("Input a city to see the weather: ")
     url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid=cb1657161a4c396d7510303571ee489f".format(city)
+    response = requests.get(url)
+    data = response.json()
+    df = pd.DataFrame({
+        'city': data['name'],
+        'country': (data['sys']['country']),
+        'temp': (data['main']['temp']),
+        'min temp': (data['main']['temp_min']),
+        'max temp': (data['main']['temp_max']),
+        'humidity': (data['main']['humidity']),
+        'pressure': (data['main']['pressure']),
+        'wind': (data['wind']['speed']),
+        'visibility': [data['visibility']]
+    })
+    return df
 
-res = requests.get(url)
-json = res.json()
-pprint(json)
+weather_data = get_weather()
+pprint(weather_data)
