733e915ebdc272c2dae7bb6ce020974a0fffc83b
vadbash <vadusikvadim@gmail.com>
2023-03-13 16:19:37 +0200

    fix pandas

--- weather.py
+++ weather.py
@@ -3,23 +3,23 @@ import json
 from pprint import pprint
 import pandas as pd
 
-def get_weather():
+def weather():
     city = input("Input a city to see the weather: ")
     url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid=cb1657161a4c396d7510303571ee489f".format(city)
-    response = requests.get(url)
-    data = response.json()
+
+    res = requests.get(url)
+    json = res.json()
     df = pd.DataFrame({
-        'city': data['name'],
-        'country': (data['sys']['country']),
-        'temp': (data['main']['temp']),
-        'min temp': (data['main']['temp_min']),
-        'max temp': (data['main']['temp_max']),
-        'humidity': (data['main']['humidity']),
-        'pressure': (data['main']['pressure']),
-        'wind': (data['wind']['speed']),
-        'visibility': [data['visibility']]
+        'city': json['name'],
+        'country': (json['sys']['country']),
+        'temp': (json['main']['temp']),
+        'min temp': (json['main']['temp_min']),
+        'max temp': (json['main']['temp_max']),
+        'humidity': (json['main']['humidity']),
+        'pressure': (json['main']['pressure']),
+        'wind': (json['wind']['speed']),
+        'visibility': [json['visibility']]
     })
     return df
-
-weather_data = get_weather()
-pprint(weather_data)
+   
+pprint(weather())
